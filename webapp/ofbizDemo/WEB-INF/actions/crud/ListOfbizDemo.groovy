06968c60c8e47f006c2abb372b1407a0ed3347b6
Suraj Singh Chouhan <surajchouhan352@gmail.com>
2021-06-23 23:19:31 +0530

    design Customizing user interface using FreeMarker template and groovy script

---
+++ webapp/ofbizDemo/WEB-INF/actions/crud/ListOfbizDemo.groovy
@@ -0,0 +1,5 @@
+
+ofbizDemoTypes = delegator.findList("OfbizDemoType", null, null, null, null, false);
+context.ofbizDemoTypes = ofbizDemoTypes;
+ofbizDemoList = delegator.findList("OfbizDemo", null, null, null, null, false);
+context.ofbizDemoList = ofbizDemoList;
